3ae67077b0ee3eb544f7bb2980049cc4db83e2db
Syeda <mishi@BOOK-PKEGQ9A764.localdomain>
2026-08-05 14:48:09 -0400

    updating the repo with LLS projects practice in mind, starting phase one

---
+++ nav.sh
@@ -0,0 +1,14 @@
+export MP_ROOT="/mnt/c/Users/mishi/OneDrive/Desktop/Github/GithubProjects/MiniProjects"
+
+home(){
+    cd "$MP_ROOT"
+}
+
+pone() {
+    cd "$MP_ROOT/LLS/PhaseOne"
+}
+
+help (){
+    echo "All Functions"
+    echo "home, pone - LLS/PhaseOne"
+}
